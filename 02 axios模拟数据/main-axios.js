f035c36594b6c51d2ef533a6edcdf6ad8311c67b
gmYuan <ygm121351@gmail.com>
2019-01-06 23:26:32 +0800

    add

--- 02 axios模拟数据/main-axios.js
+++ 02 axios模拟数据/main-axios.js
@@ -1,14 +1,18 @@
-let $number = $('#number')
-
 
 $('#addOne').on('click', function(){ 
-  $number.text(+$number.text() + 1)
+  let number = + $('#number').text() + 1
+  $('#number').text(number)
 })
 
 $('#minusOne').on('click', function(){
-  $number.text(+$number.text() - 1)
+  let number = + $('#number').text() - 1
+  $('#number').text(number)
 })
 
 $('#reset').on('click', function(){
-  $number.text(0)
+  $('#number').text(0)
 })
+
+// 反思小结:
+  // 1. 尽量避免使用全局变量，多使用局部变量
+  // 2. 对于text()这种可赋值/可获取的方法，可以先获取存入变量，再通过对象方法赋值
