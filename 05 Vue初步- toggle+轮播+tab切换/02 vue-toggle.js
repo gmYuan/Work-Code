b2add04121f4e630c735ad4ecb78fa83871a21e9
lebei_ygm <ygm121351@163.com>
2019-01-16 18:53:37 +0800

    add

---
+++ 05 Vue初步- toggle+轮播+tab切换/02 vue-toggle.js
@@ -0,0 +1,19 @@
+﻿let view = new Vue({
+  el: "#app",
+  data: {
+    seen: true
+  },
+  
+  template: `
+   <div>
+     <button v-on:click="toggle"> 点我 </button>
+     <p v-if="seen"> 你好你好 </p>
+   </div>
+  `,
+  
+  methods: {
+    toggle(){
+      this.seen = !this.seen
+    }
+  }  // 对应methods
+})
