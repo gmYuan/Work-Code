75b3463438dfb4997785b0aa33b9b3cd8b75afa0
gmYuan <ygm121351@gmail.com>
2019-06-19 08:25:22 +0800

    add

--- 11 本地mock数据+配置.vue
+++ 11 本地mock数据+配置.vue
@@ -9,13 +9,33 @@ S3.2 本地的模拟数据，可以放在 static/mock文件夹下(外部可以�
 S3.3 注意，本地的模拟数据要添加到 .gitignore里(避免提交)
 
 
-S4.1 使用本地模拟数据会有一个问题，在上线时需要访问真实API，所以需要做一个代理配置
+S4.1 使用本地模拟数据会有一个问题，在上线时需要访问真实API，而不是本地的static/mock文件夹 
 
-S4.2
+S4.2 所以需要做一个代理配置,这样在开发环境中也可以直接写真实的API地址(代理会自动转为访问 static/mock)
+
+S4.3 配置方法为: src/config/index.js文件下:
+
+module.exports = {
+  dev: {
+
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {                             //代理配置
+      '/api': {                               // 需要被代理的url
+        target: 'http://localhost:8080',
+        pathRewrite: {                        // 路径重写
+          '^/api': 'static/mock'
+        }
+      }
+    },
+
+  }
+}
+// 配置完要重启项目
 
 
-
-
+S5 创建本地JOSN数据
 
 
 
@@ -23,8 +43,6 @@ S4.2
   <div>
 
     ......
-   
-
 
   </div>
 </template>
@@ -43,13 +61,7 @@ export default {
 
   mounted () {
     this.getHomeInfo()
-  }
-  
-}
-
-    
-
-
   },
+  
 }
 </script>
