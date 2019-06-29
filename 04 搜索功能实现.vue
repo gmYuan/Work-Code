f060aff65ccd85607f4b9d5ec33214f8c68731a4
gmYuan <ygm121351@163.com>
2019-06-29 14:04:56 +0800

    add

---
+++ 04 搜索功能实现.vue
@@ -0,0 +1,16 @@
+参考文档
+
+01 [\](\)
+直接参考文档
+
+
+
+S1 设置搜索结果展示静态页面: position绝对定位 + 遮罩层效果
+
+S2 获取遍历搜索的数据源cities: props获取
+
+S3
+
+  S3.1 双向绑定input的结果searchContent，并用watch监听变化
+  S3.2 当searchContent发生变化时，遍历citite，从中查找是否有对应数据
+
